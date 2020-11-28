66c32f85b720ad3ebae95cf3167981d7f6d5a95e
ting682 <tchung682@gmail.com>
2020-11-28 18:14:28 -0500

    fixed add entries

--- src/reducers/entriesReducer.js
+++ src/reducers/entriesReducer.js
@@ -1,16 +1,16 @@
 export function entriesReducer(state = { entries: [], requesting: false}, action) {
-    
+    debugger
     switch (action.type) {
         case "START_ADDING_ENTRIES_REQUEST":
             return {
-                
+                ...state,
                 entries: [...state.entries],
                 requesting: true
             }
-        case "ADD_ENTRY":
+        case "ADD_ENTRIES":
             
             return {
-                
+                ...state,
                 entries: action.entries,
                 requesting: false
             }
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -8,3 +8,5 @@ export const rootReducer = (history) => combineReducers({
     entries: entriesReducer
   });
 
+
+
