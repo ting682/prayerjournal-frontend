c2e44f3b50918fbf9c6c0256cc14f1a3bbabd54e
ting682 <tchung682@gmail.com>
2020-12-05 17:23:32 -0500

    fixed html content

--- src/components/entry.js
+++ src/components/entry.js
@@ -1,15 +1,16 @@
 import { CommentsContainer }  from './commentsContainer'
 import TimeAgo from 'javascript-time-ago'
+import Parser from 'html-react-parser'
 
 export const Entry = (props) => {
     //debugger
     const timeAgo = new TimeAgo('en-US')
-
+    //const content = JSON.parse(props.entry.content)
     return (
         <div className="entry">
             <h3>Created by: {props.entry.name} </h3>
             <p>{timeAgo.format(new Date(props.entry.updated_at))}</p>
-            {props.entry.content}
+            {Parser(props.entry.content)}
             <p>Likes: {props.entry.likes_count}</p>
             <CommentsContainer comments={props.comments} />
         </div>
