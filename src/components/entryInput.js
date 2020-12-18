1d9f1f974363edf64e5ecbfcafed7247b31af9a7
ting682 <tchung682@gmail.com>
2020-12-17 20:16:54 -0500

    revised quill toolbar

--- src/components/entryInput.js
+++ src/components/entryInput.js
@@ -55,14 +55,14 @@ class EntryInput extends Component {
                     </Form.Group>
         */}
                     <ReactQuill value={this.state.content} onChange={this.handleChange} modules={{ toolbar: [
-                    [{ 'font': [] }, { 'size': [] }],
-                    [ 'bold', 'italic', 'underline', 'strike' ],
+                    {/*[{ 'font': [] }, { 'size': [] }], */}
+                    [ 'bold', 'italic', 'underline'], {/* 'strike' ], */}
                     [{ 'color': [] }, { 'background': [] }],
-                    [{ 'script': 'super' }, { 'script': 'sub' }],
-                    [{ 'header': '1' }, { 'header': '2' }, 'blockquote', 'code-block' ],
-                    [{ 'list': 'ordered' }, { 'list': 'bullet'}, { 'indent': '-1' }, { 'indent': '+1' }],
-                    [ 'direction', { 'align': [] }],
-                    [ 'link', 'image', 'video', 'formula' ],
+                    {/* [{ 'script': 'super' }, { 'script': 'sub' }], */}
+                    [{ 'header': '1' }, { 'header': '2' }, 'blockquote'], {/* , 'code-block' ], */}
+                    [{ 'list': 'ordered' }, { 'list': 'bullet'}], {/* { 'indent': '-1' }, { 'indent': '+1' }], 
+                    [ 'direction', { 'align': [] }],*/}
+                    [ 'link', 'image', 'video'], {/* 'formula' ], */}
                     [ 'clean' ]]}} />
                     
                         <Form.Group>
