a9a25ab8b685e68b2d2b9b73b18753d8ed37368f
ting682 <tchung682@gmail.com>
2020-12-29 10:20:26 -0500

    added video

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,12 @@
 import { OurMannaVerse } from './ourMannaVerse'
+import { IntroVideo } from './introVideo'
 
 export const Home = (props) => {
 
     //debugger
     return (
         <div>
+            <IntroVideo />
             <h1>Welcome to the Prayer Journal app!</h1>
             <p>
                 This website is made for people who want to keep a prayer journal. Also, we have a prayer wall so all who participate can pray together!
---
+++ src/components/introVideo.js
@@ -0,0 +1,21 @@
+import React, { useRef, useEffect } from 'react'
+
+export function IntroVideo (props) {
+
+    const vidRef = useRef(null)
+
+    const handlePlay = (e) => {
+        vidRef.current.play()
+    }
+    
+    return (
+        <React.Fragment>
+            <video id="video" autoPlay={true} loop width="100%" ref={vidRef} height="100%" >
+                <source src="prayer_journal_intro.mp4" type="video/mp4" />
+            </video>
+            <button onClick={handlePlay}>Play</button>
+        </React.Fragment>
+        
+        
+    )
+}
