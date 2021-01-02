ffdde9fb96db54fa019462ddda01e4e03f1efbce
ting682 <tchung682@gmail.com>
2021-01-02 11:29:23 -0500

    fixed link

--- src/components/introCard.js
+++ src/components/introCard.js
@@ -4,8 +4,8 @@ export const IntroCard = (props) => {
 
 
     return (
-        <Card style={{width: '18rem'}}>
-            <Card.Img variant='top' href={props.image}/>
+        <Card className="introcard" style={{width: '18rem'}}>
+            <Card.Img variant='top' src={props.image}/>
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
                 <Card.Text>
