8dcc12e0cd58c0cf397bd332974aeee4c9d4458f
ting682 <tchung682@gmail.com>
2021-01-02 10:07:36 -0500

    fb icon

--- src/components/fbShare.js
+++ src/components/fbShare.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
 export default function FbShare (props) {
 
     const url = 'https://prayerjournal.place/'
@@ -5,6 +8,13 @@ export default function FbShare (props) {
     const entryRoute = `https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fprayerjournal.place%2Fentries%2F${props.entryRoute}&amp;src=sdkpreparse`
 
     return (
-        <div class="fb-share-button" data-href={url + props.route} data-layout="button" data-size="small"><a target="_blank" href={entryRoute} class="fb-xfbml-parse-ignore">Share</a></div>
+        <React.Fragment>
+
+        
+        <FontAwesomeIcon icon={['fab', 'facebook']} />
+            <div class="fb-share-button" data-href={url + props.route} data-layout="button" data-size="small"><a target="_blank" href={entryRoute} class="fb-xfbml-parse-ignore">Share</a></div>
+
+            </React.Fragment>
+        
     )
 }
