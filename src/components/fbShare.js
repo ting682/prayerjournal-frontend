6ec23455bfdac59680335c5704769681b238a560
ting682 <tchung682@gmail.com>
2021-01-02 08:55:17 -0500

    changed share link

--- src/components/fbShare.js
+++ src/components/fbShare.js
@@ -5,6 +5,6 @@ export default function FbShare (props) {
     const entryRoute = `https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fprayerjournal.place%2Fentries%2F${props.entryRoute}&amp;src=sdkpreparse`
 
     return (
-        <div class="fb-share-button" data-href={url + props.route} data-layout="button_count" data-size="small"><a target="_blank" href={entryRoute} class="fb-xfbml-parse-ignore">Share</a></div>
+        <div class="fb-share-button" data-href={url + props.route} data-layout="button" data-size="small"><a target="_blank" href={entryRoute} class="fb-xfbml-parse-ignore">Share</a></div>
     )
 }
